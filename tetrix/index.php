a029e6c2f6af9821504e095019ca5cef75947e07
Michael Buckley <michael.buckley@cordatahealth.com>
2016-02-14 21:59:35 -0500

    Recreate tetrix branch the right way this time

---
+++ tetrix/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+
+  <head>
+
+    <title>
+      Tetrix
+    </title>
+
+    <?php include("../common/head.html"); ?>
+
+    <link rel="stylesheet" type="text/css" href="tetrix.css">
+    <script type="text/javascript" src="tetrix.js"></script>
+
+  </head>
+
+  <body onload="start()">
+
+    <?php include("../common/nav.html"); ?>
+
+    <div class="content">
+
+      <canvas id="tetrix"></canvas>
+
+    </div>
+
+    <?php include("../common/footer.html"); ?>
+
+  </body>
+
+</html>
